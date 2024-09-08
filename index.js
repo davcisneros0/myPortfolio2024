9ebf590cfc6b77c288b857e4c6d2943c1dce0637
David Cisneros <davcisneros2000@gmail.com>
2024-09-08 02:30:58 -0400

    Add JS and CSS for hamburger menu functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+              //NAVIGATION BAR JAVASCRIPT//
+let mainMenu = document.querySelector('.mainMenu');
+let closeMenu = document.querySelector('.closeMenu');
+let openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top = "-100%";
+}
+  
